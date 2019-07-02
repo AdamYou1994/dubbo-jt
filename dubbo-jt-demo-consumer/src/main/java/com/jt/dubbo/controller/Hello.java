28a10b01e58622a7dc98c58136b3e3df7e5a568e
UID <1213143892@qq.com>
2019-07-02 12:44:24 +0800

    test first

--- dubbo-jt-demo-consumer/src/main/java/com/jt/dubbo/controller/Hello.java
+++ dubbo-jt-demo-consumer/src/main/java/com/jt/dubbo/controller/Hello.java
@@ -2,6 +2,6 @@ package com.jt.dubbo.controller;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("test ..... git ");
+		System.out.println("test ..... first ");
 	}
 }
